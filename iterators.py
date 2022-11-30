36f7157360678c7a4839b32d61fb854b64a4d338
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-12-01 00:08:57 +0500

    python iterator tricks

---
+++ iterators.py
@@ -0,0 +1,132 @@
+# import time
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class MyDataStructure:
+
+    def __init__(self, value1, value2):
+        self.value1 = value1
+        self.value2 = value2
+        self.counter = 0
+
+    def __iter__(self):
+        self.counter = 0
+        return self
+
+    def __next__(self):
+        if self.counter > 1:
+            raise StopIteration
+        value = self.value1 if self.counter == 0 else self.value2
+        self.counter += 1
+        return value
+
+
+# for i in MyDataStructure(1, 2):
+#     print(i)
+
+
+class MyArray:
+
+    def __init__(self, *values: int):
+        self.check_data_type(*values)
+        self.data = values
+        self.index = 0
+
+    @staticmethod
+    def check_data_type(*values):
+        for data in values:
+            if not isinstance(data, int):
+                raise ValueError(f"Expected 'int': Got '{type(data).__name__}'")
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index == len(self.data):
+            raise StopIteration
+        value = self.data[self.index]
+        self.index += 1
+        return value
+
+
+# for i in MyArray(1, 2, 3, 4, 5, True):
+#     print(i)
+
+
+# print(MyArray(1, 2).__next__())  # U can define and __next__()/next() without defining
+# __iter__ on class, cause __iter__ is just for "for" loop
+
+# An iterable is an object that has an __iter__ method which returns an iterator, or which defines a __getitem__
+# method that can take sequential indexes starting from zero (and raises an IndexError when the indexes are no
+# longer valid). So an iterable is an object that you can get an iterator from.
+
+# An iterator is an object with a next (Python 2) or __next__ (Python 3) method.
+
+
+class MyRange:
+
+    def __init__(self, start: int, end: int):
+        self.start = start
+        self.end = end
+        self.counter = self.start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.counter == self.end:
+            raise StopIteration
+        value = self.counter
+        self.counter += 1
+        return value
+
+    def __str__(self):
+        return f"MyRange({self.start}, {self.end})"
+
+    def __repr__(self):
+        return f"MyRange({self.start}, {self.end})"
+
+
+# print(MyRange(20, 30))
+# for i in MyRange(10, 20):
+#     print(i)
+
+
+# for i in requests.get("https://habr.com").iter_lines():
+#     print(i.decode("utf-8"))
+
+# requests.Response.iter_lines() returns an iterator that iterates through the rows of content
+
+
+# print(requests.get("https://habr.com").text.__sizeof__())
+# counter = 0
+# for i in requests.get("https://habr.com", stream=True).iter_lines(chunk_size=300):
+#     print(i.decode("utf-8"), "end!!!")
+# counter += i.decode("utf-8").__sizeof__()
+# print(counter)
+
+
+# class NoEffectiveGithubParserIterator:
+#     """Yes, it's iterator, but code logic is no effect"""
+#     target_url = "https://github.com/SaD-Pr0gEr?tab=repositories"
+#
+#     def __init__(self):
+#         self.iter_content = requests.get(self.target_url, stream=True).iter_lines()
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         content = BeautifulSoup(
+#             next(self.iter_content).decode("utf-8"),
+#             "html.parser"
+#         )
+#         repo_obj = content.find("a", attrs={"itemprop": "name codeRepository"})
+#         return f'https://github.com{repo_obj["href"]}' if repo_obj else None
+#
+#
+# for data in NoEffectiveGithubParserIterator():
+#     if data: print(data)
